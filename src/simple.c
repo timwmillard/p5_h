a5da7e71a9ae4752651cc9177dce38236840ad94
Tim Millard <timwmillard@gmail.com>
2025-07-06 15:57:29 +1000

    simple demo

---
+++ src/simple.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+// sokol dependencies
+#include "sokol_app.h"
+#include "sokol_gfx.h"
+#include "sokol_glue.h"
+#include "sokol_gp.h"
+
+// p5 library with implementation
+#define P5_IMPLEMENTATION
+#include "../p5.h"
+
+void setup() {
+}
+
+void draw() {
+}
+
+P5_MAIN(640, 480, "Handmade");
